4601e37259e7810666a2f6f7d1bf0a0e0ef031e6
Paulo Véliz <pvelizs16@gmail.com>
2023-02-05 01:31:00 -0500

    3er version endpoint form_reclamo works

--- src/pages/api/form/form_reclamo.ts
+++ src/pages/api/form/form_reclamo.ts
@@ -12,25 +12,50 @@ export default async function handler(
     res: NextApiResponse<any>
 ) {
 
+    if (req.method !== 'POST') {
+        return res.status(405).json({
+            message: 'Only POST requests are allowed.'
+        });
+    }
+
     try {
         const body =req.body
 
-        const last_item: Reclamo | null = await prisma.reclamo.findFirst({      // Corregir Reclamo cuando cambie modelo
-            orderBy: {
-                id: 'desc',
-            }
-        });
+        let last_item: Reclamo | null;
 
-        let id_reclamo = 0;
+        if (body.tipo_reclamo === 'RECLAMO') {
+            last_item = await prisma.reclamo.findFirst({
+                where: {
+                    tipo_reclamo: 'RECLAMO'
+                },
+                orderBy: {
+                    id: 'desc'
+                }
+            });
 
-        if (last_item != null){
-            id_reclamo += last_item.id + 1;
-        } else {
-            id_reclamo += 1;
+        } else if (body.tipo_reclamo === 'QUEJA') {
+            last_item = await prisma.reclamo.findFirst({
+                where: {
+                    tipo_reclamo: 'QUEJA'
+                },
+                orderBy: {
+                    id: 'desc'
+                }
+            });
         }
 
+        let id_item = 0;
+
+        if (last_item != null){
+
+            id_item += parseInt(last_item.nro_seguimiento.substring(2,9)) + 1;
+
+        } else {
+            id_item += 1;
+        };
+
         console.log(last_item);
-        console.log(id_reclamo);
+        console.log(id_item);
 
         const date_now: Date = new Date();
         const year = date_now.getFullYear();
@@ -38,83 +63,74 @@ export default async function handler(
         console.log((year));
 
         let detalle = ''
-        if (body.detalle_reclamo != null){
-            detalle = 'Reclamo';
+        if (body.detalle_reclamo != null && body.tipo_reclamo === 'RECLAMO' ){
+            detalle = 'R';
 
         } else {
-            detalle = 'Queja';
-        }
-        // 300001-2023 Queja    id = 1
-        // 500001-2023 Reclamo  id = 2
-        // 500002-2023 Reclamo  id = 3
-        // 500003-2023 Reclamo  id = 4
-        // 300002-2023 Queja    id = 5
+            detalle = 'Q';
+        };
 
-        const nro_seguimiento  = `${detalle} Nº 00000000${id_reclamo}-${year}`;
+        let digit_id = id_item.toString().length;
+        let num_seg = '0'.repeat(7-digit_id) + id_item.toString();
+
+        const nro_seguimiento  = `${detalle}-${num_seg}-${year}`;
         
         console.log((nro_seguimiento));
 
+        const data_common = {
+            nro_seguimiento,
+            nombres: body.nombres,
+            apellidos: body.apellidos,
+            mayor_edad: body.mayor_edad,
+            tipo_documento: body.tipo_documento,
+            nro_dni: body.nro_dni,
+            nombre_apoderado: body.nombre_apoderado,
+            direccion: body.direccion,
+            referencia: body.referencia,
+            departamento: body.departamento,
+            provincia: body.provincia,
+            distrito: body.distrito,
+            email: body.email,
+            telefono: body.telefono,
+            tipo_reclamo: body.tipo_reclamo,
+            motivo_reclamo: body.motivo_reclamo,
+            pedido_reclamo: body.pedido_reclamo,
+            estado_reclamo: body.estado_reclamo,
+            empresa: {connect: {
+                id: body.empresaId
+            }}
+        };
+
         let data:Prisma.ReclamoCreateInput;
+        let common = ''
+
         if (body.detalle_reclamo != null) {
             data = {
-                nro_seguimiento,
-                nombres: body.nombres,
-                apellidos: body.apellidos,
-                mayor_edad: body.mayor_edad,
-                tipo_documento: body.tipo_documento, // Podría obiarse porque tiene por defecto DNI
-                nro_dni: body.nro_dni,
-                nombre_apoderado: body.nombre_apoderado, // Podría ser null hacia abajo
-                direccion: body.direccion,
-                referencia: body.referencia,
-                departamento: body.departamento,
-                provincia: body.provincia,
-                distrito: body.distrito,                 // Pdría ser null hacia arriba
-                email: body.email,
-                telefono: body.telefono,
-                tipo_reclamo: body.tipo_reclamo,     // Podría obiarse porque tiene por defecto DNI
-                motivo_reclamo: body.motivo_reclamo,
-                pedido_reclamo: body.pedido_reclamo,
-                estado_reclamo: body.estado_reclamo, // Podría obiarse porque tiene por defecto DNI
+                ...data_common,
                 detalle_reclamo: {
                     create: {
-                        fecha_compra: new Date(body.detalle_reclamo.fecha_compra), // Cómo ingresar la fecha
-                        tipo_comprobante: body.detalle_reclamo.tipo_comprobante, // Podría obiarse porque tiene por defecto DNI
+                        fecha_compra: new Date(body.detalle_reclamo.fecha_compra),
+                        tipo_comprobante: body.detalle_reclamo.tipo_comprobante,
                         nro_comprobante: body.detalle_reclamo.nro_comprobante,
                         monto_producto: body.detalle_reclamo.monto_producto,
                         codigo_producto: body.detalle_reclamo.codigo_producto,
                         detalle_producto: body.detalle_reclamo.detalle_producto
                     }
-                },
-                empresa: body.empresaId
+                }
             }
+
+            common = 'Reclamo';
+
         } else {
-            data = {
-                nro_seguimiento,
-                nombres: body.nombres,
-                apellidos: body.apellidos,
-                mayor_edad: body.mayor_edad,
-                tipo_documento: body.tipo_documento, // Podría obiarse porque tiene por defecto DNI
-                nro_dni: body.nro_dni,
-                nombre_apoderado: body.nombre_apoderado, // Podría ser null hacia abajo
-                direccion: body.direccion,
-                referencia: body.referencia,
-                departamento: body.departamento,
-                provincia: body.provincia,
-                distrito: body.distrito,                 // Pdría ser null hacia arriba
-                email: body.email,
-                telefono: body.telefono,
-                tipo_reclamo: body.tipo_reclamo,     // Podría obiarse porque tiene por defecto DNI
-                motivo_reclamo: body.motivo_reclamo,
-                pedido_reclamo: body.pedido_reclamo,
-                estado_reclamo: body.estado_reclamo, // Podría obiarse porque tiene por defecto DNI
-                empresa: body.empresaId
-            }
+            data = data_common;
+
+            common = 'Queja';
         }
 
-        const reclamo: Reclamo = await prisma.reclamo.create({data});
+        const reclamo: Reclamo = await prisma.reclamo.create({ data });
 
         return res.status(201).json({
-            message: "Reclamo creado correctamente.",
+            message: `${common} se creó correctamente.`,
             ok:true
         });
         
