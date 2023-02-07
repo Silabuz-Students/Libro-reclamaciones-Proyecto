aba61a33dfe498bd49a71dcff2ca57c37033f5e5
MarieSFloresM <78627002+mariesfloresmoran@users.noreply.github.com>
2023-02-06 23:00:36 -0500

    No need to use fecha de reclamo

--- src/pages/form/formulario.tsx
+++ src/pages/form/formulario.tsx
@@ -37,15 +37,6 @@ export default function Document() {
                                 </div>
 
                                 <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                    <label htmlFor="last_name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                        Fecha de Reclamo
-                                    </label>
-                                    <div className="mt-1 sm:mt-0 sm:col-span-2">
-                                    <input type="text" id="last_name"  required className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
-                                    </div>
-                                </div>
-
-                                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                     <label htmlFor="oldenougth" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                         Mayor de edad
                                     </label>
